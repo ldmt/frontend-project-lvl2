0a60ad71c6d998897ce07cf8beeda64ea0bc152b
Leonid Matrenin <lndmtrnn@gmail.com>
2022-07-18 21:51:30 +0300

    create test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,23 @@
-/* eslint-disable import/extensions */
-import { test, expect } from 'jest';
-import gendiff from '../index.js';
+import { test, expect } from '@jest/globals';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/index.js';
 
-test('gendiff', () => {
-  expect(gendiff());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+// const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('genDiff', () => {
+  const path1 = getFixturePath('file1.json');
+  const path2 = getFixturePath('file2.json');
+
+  expect(genDiff(path1, path2)).toBe(`{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true\n}`);
 });
