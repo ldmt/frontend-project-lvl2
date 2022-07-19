46f347361e58f8bd03b2b4a05195f8a9ab1b75cd
Leonid Matrenin <lndmtrnn@gmail.com>
2022-07-19 03:35:52 +0300

    add parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-prototype-builtins */
 import * as fs from 'fs';
+import { extname } from 'path';
+import parse from './parser.js';
 
-const diff = (dt1, dt2) => {
-  const obj1 = JSON.parse(dt1);
-  const obj2 = JSON.parse(dt2);
+const diff = (data) => {
+  const { parsedData1: obj1, parsedData2: obj2 } = data;
 
   const keys1 = Object.keys(obj1).sort();
   const keys2 = Object.keys(obj2).sort();
@@ -41,8 +42,15 @@ const diff = (dt1, dt2) => {
 export default (fp1, fp2, format) => {
   const data1 = fs.readFileSync(fp1);
   const data2 = fs.readFileSync(fp2);
+  const data = { data1, data2 };
 
-  const result = diff(data1, data2);
+  const fileExt1 = extname(fp1);
+  const fileExt2 = extname(fp2);
+  const fileExt = { fileExt1, fileExt2 };
+
+  const parsedData = parse(data, fileExt);
+
+  const result = diff(parsedData);
 
   return result;
 };
