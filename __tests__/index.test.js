60d9d9cfac7f7ec3db9c8df097da76a2c2fea680
Leonid Matrenin <lndmtrnn@gmail.com>
2022-07-26 03:08:33 +0300

    create tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,48 +9,77 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 // const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('genDiff only JSON', () => {
-  const path1 = getFixturePath('file1.json');
-  const path2 = getFixturePath('file2.json');
+test('genDiff only nested JSON', () => {
+  const path1 = getFixturePath('nestedFile1.json');
+  const path2 = getFixturePath('nestedFile2.json');
 
   expect(genDiff(path1, path2)).toBe(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true\n}`);
+   common: {
+     + follow: false
+       setting1: Value 1
+     - setting2: 200
+     - setting3: true
+     + setting3: null
+     + setting4: blah blah
+     + setting5: {
+           key5: value5
+       }
+       setting6: {
+           doge: {
+             - wow: 
+             + wow: so much
+           }
+           key: value
+         + ops: vops
+       }
+   }
+   group1: {
+     - baz: bas
+     + baz: bars
+       foo: bar
+     - nest: {
+           key: value
+       }
+     + nest: str
+   }
+ - group2: {
+       abc: 12345
+       deep: {
+           id: 45
+       }
+   }
+ + group3: {
+       deep: {
+           id: {
+               number: 45
+           }
+       }
+       fee: 100500
+   }\n}`);
 });
 
-test('genDiff only YAML', () => {
-  const path1 = getFixturePath('newFile2.yml');
-  const path2 = getFixturePath('newFile.yaml');
+test('genDiff only nested YAML', () => {
+  const path1 = getFixturePath('nestedYaml1.yaml');
+  const path2 = getFixturePath('nestedYaml2.yml');
 
   expect(genDiff(path1, path2)).toBe(`{
-    doe: a deer, a female deer
-  + follow: true
-  - french-hens: 10
-  + french-hens: 3
-    pi: 3.14159
-  - ray: a drop
-  + ray: a drop of golden sun
-  - xmas: false
-  + xmas: true\n}`);
-});
-
-test('genDiff mixed', () => {
-  const path1 = getFixturePath('file1.json');
-  const path2 = getFixturePath('newFile.yaml');
-
-  expect(genDiff(path1, path2)).toBe(`{
-  + doe: a deer, a female deer
-  - follow: false
-  + follow: true
-  + french-hens: 3
-  - host: hexlet.io
-  + pi: 3.14159
-  - proxy: 123.234.53.22
-  + ray: a drop of golden sun
-  - timeout: 50
-  + xmas: true\n}`);
+   doe: a deer, a female deer
+ - french-hens: 8
+ + french-hens: 5
+   pi: 3.14159
+ - ray: a drop
+ + ray: a drop of golden sun
+ - xmas: null
+ + xmas: true
+   xmas-fifth-day: {
+       calling-birds: four
+       french-hens: 3
+       golden-rings: 5
+       partridges: {
+           count: 2
+         - location: a pear tree
+         + location: a pear
+       }
+       turtle-doves: two
+   }\n}`);
 });
