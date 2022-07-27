8bfe6cfcb34973b1d3a1519cadca09dfaeb2bac1
Leonid Matrenin <lndmtrnn@gmail.com>
2022-07-28 02:11:01 +0300

    add json formatter

--- formatters/index.js
+++ formatters/index.js
@@ -1,11 +1,15 @@
+/* eslint-disable consistent-return */
 /* eslint-disable no-else-return */
 import stylish from './stylish.js';
 import plain from './plain.js';
+import toJSON from './formatJSON.js';
 
 export default (frt) => {
   if (frt === 'stylish') {
     return stylish;
   } else if (frt === 'plain') {
     return plain;
+  } else if (frt === 'json') {
+    return toJSON;
   }
 };
