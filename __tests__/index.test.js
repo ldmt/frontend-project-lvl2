754634ac69796f882e9771ca9b029002c6a095a7
Leonid Matrenin <lndmtrnn@gmail.com>
2022-07-28 01:29:55 +0300

    add test plain format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -83,3 +83,20 @@ test('genDiff only nested YAML', () => {
        turtle-doves: two
    }\n}`);
 });
+
+test('genDiff format plain JSON', () => {
+  const path1 = getFixturePath('nestedFile1.json');
+  const path2 = getFixturePath('nestedFile2.json');
+
+  expect(genDiff(path1, path2, 'plain')).toBe(`Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`);
+});
